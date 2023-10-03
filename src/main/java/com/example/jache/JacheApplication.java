807fc2f6288d7f4fa8a5aaf2cf3eef442233f92f
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2023-10-03 14:20:03 +0900

    git test

---
+++ src/main/java/com/example/jache/JacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.jache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jache/JacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
