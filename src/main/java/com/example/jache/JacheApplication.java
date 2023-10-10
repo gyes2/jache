aefd69b63f5399bd65d454ed8615279c93366393
JaeMin <90915044+Min5107@users.noreply.github.com>
2023-10-10 21:49:14 +0900

    [feat][!hotfix] 로그인 회원가입 메인 페이지 구성
    [feat]
    로그인 페이지
    회원가입 페이지
    메인 페이지 구성 완료
    
    [!hotfix]
    인텔리제이에서 폰트를 적용하지못하는 버그 발생 수정해야함

--- src/main/java/com/example/jache/JacheApplication.java
+++ src/main/java/com/example/jache/JacheApplication.java
@@ -2,8 +2,9 @@ package com.example.jache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class JacheApplication {
 
 	public static void main(String[] args) {
