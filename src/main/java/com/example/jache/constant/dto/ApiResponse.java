c37f254f7ec6abd1603a3bc0746f197030078dee
DESKTOP-FA2LLMO\jieun <jieunlee0502@naver.com>
2023-10-10 16:50:28 +0900

    feat: 회원가입 기능 구현 중

---
+++ src/main/java/com/example/jache/constant/dto/ApiResponse.java
@@ -0,0 +1,73 @@
+package com.example.jache.constant.dto;
+
+import com.example.jache.constant.enums.CustomResponseStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.example.jache.constant.enums.CustomResponseStatus.SUCCESS;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@JsonPropertyOrder({"isSuccess","code","message","data"})
+public class ApiResponse<T> {
+    @JsonProperty(("isSuccess"))
+    private boolean isSuccess;
+    private int code;
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static <T> ApiResponse<T> createSuccess(T data, CustomResponseStatus status) {
+        return new ApiResponse<>(true, status.getCode(),status.getMessage(), data);
+    }
+
+    public static ApiResponse<?> createSuccessWithNoContent(CustomResponseStatus status) {
+        return new ApiResponse<>(true, status.getCode(),status.getMessage(), null);
+    }
+
+    // Hibernate Validator에 의해 유효하지 않은 데이터로 인해 API 호출이 거부될때 반환
+    public static ApiResponse<?> createFail(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+        for (ObjectError error : allErrors) {
+            if (error instanceof FieldError) {
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage());
+            } else {
+                errors.put( error.getObjectName(), error.getDefaultMessage());
+            }
+        }
+        return new ApiResponse<>(false, HttpStatus.BAD_REQUEST.value(),"유효하지 않은 데이터입니다.", errors);
+    }
+
+    // 예외 발생으로 API 호출 실패시 반환
+    public static ApiResponse<?> createError(CustomResponseStatus status) {
+        return new ApiResponse<>(status.isSuccess(),status.getCode(),status.getMessage(), null);
+    }
+
+    private ApiResponse(CustomResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.code = status.getCode();
+        this.message = status.getMessage();
+    }
+    private ApiResponse(T data){
+        this.isSuccess = SUCCESS.isSuccess();
+        this.code = SUCCESS.getCode();
+        this.message = SUCCESS.getMessage();
+        this.data = data;
+    }
+}
--- src/main/java/com/example/jache/user/controller/ChefController.java
+++ src/main/java/com/example/jache/user/controller/ChefController.java
@@ -1,5 +1,7 @@
 package com.example.jache.user.controller;
 
+import com.example.jache.constant.dto.ApiResponse;
+import com.example.jache.constant.enums.CustomResponseStatus;
 import com.example.jache.user.dto.ChefDto;
 import com.example.jache.user.service.ChefService;
 import com.example.jache.user.service.ChefServiceImpl;
@@ -7,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,10 +22,16 @@ public class ChefController {
         log.info(signup.toString());
         //chefService에서 회원가입 메소드 호출
         ChefDto.SignUpResponseDto registerSignup = chefService.saveChef(signup);
-        model.addAttribute("signupResponseDto",registerSignup);
+        model.addAttribute("signupResponseDto", ApiResponse.createSuccess(registerSignup, CustomResponseStatus.SUCCESS));
         return "main";
     }
 
+    @GetMapping("/check/name/{chefname}")
+    public String checkdupChefName(Model model, @RequestParam String chefname){
+        boolean bool = chefService.checkDuplicateCheckName(chefname);
+        model.addAttribute("checkChefName",ApiResponse.createSuccess(bool,CustomResponseStatus.SUCCESS));
+        return "/api/join";
+    }
     @GetMapping("/signin")
     public String signin(Model model, @RequestBody ChefDto.SigninRequestDto signin){
 
