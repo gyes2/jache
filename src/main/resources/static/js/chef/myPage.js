8c867d200f9bda3268a7ffd3c94117e4ac1211d5
HARA1029 <gusdud4929@gmail.com>
2023-10-11 09:25:27 +0900

    feat:마이페이지 구성

---
+++ src/main/resources/static/js/chef/myPage.js
@@ -0,0 +1,15 @@
+function myList() {
+    location.href="myList.html";
+}
+
+function likeReceipe() {
+    location.href="likeReceipe.html";
+}
+
+function chat() {
+    location.href="../chat/chat.html";
+}
+
+function infoEdit() {
+    location.href="infoEdit.html";
+}
