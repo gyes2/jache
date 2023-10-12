2a35d463a0adb3fdc68891228d553e7f0343abfd
HARA1029 <gusdud4929@gmail.com>
2023-10-12 10:25:21 +0900

    feat:마이페이지 구성 - 세부 페이지 목록만

---
+++ src/main/resources/static/js/chat/chat.js
@@ -0,0 +1,15 @@
+function myList() {
+    location.href="../chef/myList.html";
+}
+
+function likeReceipe() {
+    location.href="../chef/likeReceipe.html";
+}
+
+function chat() {
+    location.href="../chat/chat.html";
+}
+
+function infoEdit() {
+    location.href="../chef/infoEdit.html";
+}
