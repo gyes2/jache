059a8dd75acdcf2f76d13b88a96b1599634d944f
JaeMin <90915044+Min5107@users.noreply.github.com>
2023-10-19 21:09:54 +0900

    [!Hotfix] file 연동 오류 receipeform.js

--- src/main/resources/static/js/chef/registerfun.js
+++ src/main/resources/static/js/chef/registerfun.js
@@ -1,75 +1,97 @@
-const checkEmail = document.getElementById("reg-email");
-const checkName = document.getElementById("reg-userName");
-function checkChefEmail(chefemail) {
-    $.ajax({
-        url: '/api/email-verification/',
-        type: 'POST',
-        dataType: 'json',
-        data: {email: chefemail},
-        success: function (response) {
-            // 성공적으로 데이터를 받아온 경우
-            console.log(response);
-
-            // 예: 서버로부터 받은 응답을 기반으로 UI 변경
-            if (response.isAvailable) {
-                alert('인증 메일 전송!');
-            } else {
-                alert('인증 메일 실패.');
+let checkEmail = document.getElementById("reg-email");
+let checkName = document.getElementById("reg-userName");
+let checkEmailNum = "";
+let isEmailVerified = false;
+let isNameAvailable = false;
+async function checkChefEmail() {
+    try {
+        const response = await fetch(`/api/all/email-verification/${checkEmail.value}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
             }
-        },
-        error: function (error) {
-            // 에러 발생 시
-            console.error('Error fetching data:', error);
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        if (data.isSuccess) {
+            alert('인증 메일 전송!');
+            checkEmailNum = data.data;
+
+        } else {
+            alert('인증 메일 실패.');
         }
-    });
-}
-    checkChefEmail(checkEmail.value);
-
-
-function checkChefName(chefname) {
-    $.ajax({
-        url: '/api/check/name/' + chefname,
-        type: 'GET',
-        dataType: 'json',
-        success: function(response) {
-            // 성공적으로 데이터를 받아온 경우
-            console.log(response);
-
-            // 예: 서버로부터 받은 응답을 기반으로 UI 변경
-            if (response.isAvailable) {
-                alert('사용 가능한 이름입니다!');
-            } else {
-                alert('이미 사용 중인 이름입니다.');
-            }
-        },
-        error: function(error) {
-            // 에러 발생 시
-            console.error('Error fetching data:', error);
-        }
-    });
-
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 
+async function checkChefName() {
+    try {
+        const response = await fetch(`/api/all/check/name/${checkName.value}`);
 
-checkChefName(checkName.value);
+        const data = await response.json();
+        console.log(data);
 
-document.getElementById('register-form-id').addEventListener('submit', function(event) {
-    // 폼 제출 동작 중단
-    event.preventDefault();
+        if (data.isSuccess) {
+            alert('사용 가능한 이름입니다!');
+            isNameAvailable = true;
+        } else {
+            alert('이미 사용 중인 이름입니다.');
+            isNameAvailable = false;
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
 
-    // 폼 데이터를 가져옵니다.
-    let formData = new FormData(this);
+function checkEmailAuth(){
+    let emailAuthNumber = document.getElementById("reg-authNumber")
+    if(emailAuthNumber.value === checkEmailNum){
+        alert('이메일 인증 완료!');
+        isEmailVerified = true;
+    } else {
+        alert('다시 확인해 주세요.');
+        isEmailVerified = false;
+    }
+}
 
-    // fetch API를 사용하여 비동기 요청을 보냅니다.
+function finalRegCheck() {
+    if (!isEmailVerified || !isNameAvailable) {
+        alert('이메일 인증 또는 이름 확인을 통과하지 못했습니다. 다시 확인해주세요.');
+    } else {
+        sendRegData()
+    }
+
+}
+
+function sendRegData(){
+    let phone = document.getElementById("reg-phone").value;
+    let email = document.getElementById("reg-email").value;
+    let password= document.getElementById("reg-password").value;
+    let chefName = document.getElementById("reg-userName").value;
+
+    const data = {
+        "chefName": chefName,
+        "password": password,
+        "phone": phone,
+        "email": email
+    };
+
     fetch('/api/all/join', {
         method: 'POST',
-        body: formData
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
     })
         .then(response => response.json()) // 예상되는 응답 형식이 JSON이라고 가정합니다.
         .then(data => {
             if (data.success) {
                 // 로그인이 성공한 경우, 여기에 코드를 추가하세요.
+                window.location.href = '/main';
                 alert('가입 완료!');
             } else {
                 // 로그인이 실패한 경우, 여기에 코드를 추가하세요.
@@ -80,4 +102,6 @@ document.getElementById('register-form-id').addEventListener('submit', function(
             console.error('Error:', error);
             alert('오류가 발생했습니다. 다시 시도해주세요.');
         });
-});
+}
+
+
