a5669f59609573c4271f6b8d80058b8510b559ce
JaeMin <90915044+Min5107@users.noreply.github.com>
2023-10-21 15:27:54 +0900

    [feat] 메인 및 상세페이지 하트 로직 완료

--- src/main/resources/static/js/receipe/detailReceipe.js
+++ src/main/resources/static/js/receipe/detailReceipe.js
@@ -7,50 +7,7 @@ const receipeId = urlParams.get('receipeId');
 let deleteButton = document.getElementById("receipe-delete");
 console.log(receipeId);
 
-window.addEventListener("DOMContentLoaded", async function () {
-
-    deleteButton.addEventListener('click', async function(event) {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer " + token);
-
-        var requestOptions = {
-            method: 'DELETE',
-            headers: myHeaders,
-            redirect: 'follow'
-        };
-
-        let responses = await fetch("http://localhost:8080/api/user/receipe/delete/" + receipeId, requestOptions)
-
-        if (!responses.ok) {
-            const errorData = await responses.json();
-            alert("데이터 조회 실패: " + JSON.stringify(errorData));
-        } else {
-            window.location.href = '/main';
-            alert("삭제 완료!");
-        }
-    });
-
-    let check = await fetchReceipeDetails();
-    let returnedStatus = await heartStatus();
-    const likeBtn = document.querySelector('.heart');
-    const likedBtn = document.querySelector('.heart-liked');
-
-    if (returnedStatus === "N") {
-        // 좋아요 안한 상태
-        likeBtn.style.display = "inline-block"; // 빈 하트 표시
-        likedBtn.style.display = "none"; // 클릭된 하트 숨기기
-    } else {
-        // 현재 좋아요한 상태
-        likeBtn.style.display = "none"; // 빈 하트 숨기기
-        likedBtn.style.display = "inline-block"; // 클릭된 하트 표시
-    }
-
-    await receipeCheck(check);
-
-
-});
-
+// 상세 정보 불러오기
 async function fetchReceipeDetails() {
     try {
         // API URL을 설정합니다. 실제 API 주소로 수정해주세요.
@@ -69,125 +26,134 @@ async function fetchReceipeDetails() {
         const response = await fetch(apiUrl, requestOptions);
 
         if (!response.ok) {
-            const errorData = await response.json();
-            alert("데이터 조회 실패: " + JSON.stringify(errorData));
+            window.location.href = '/main';
         } else {
             const data = await response.json();
 
-    console.log(data);
-    // 데이터를 HTML 요소에 할당합니다.
-    document.querySelector('input[id="receipe-title"]').value = data.data.title;
-    document.querySelector('input[name="작성자"]').value = data.data.chefName;
-    document.querySelector('input[name="카테고리"]').value = data.data.theme;
-    document.querySelector('input[name="레시피 소개"]').value = data.data.introduce;
-    //document.querySelector('.receipe-detail-img').src = data.data.receipeImgUrl;
-    let receipeImg = document.getElementById('receipe-img');
-    console.log(data.data.receipeImgUrl);
-    let imgUrl = data.data.receipeImgUrl;
-    receipeImg.src = imgUrl;
+            console.log(data);
+            // 데이터를 HTML 요소에 할당합니다.
+            document.querySelector('input[id="receipe-title"]').value = data.data.title;
+            document.querySelector('input[name="작성자"]').value = data.data.chefName;
+            document.querySelector('input[name="카테고리"]').value = data.data.theme;
+            document.querySelector('input[name="레시피 소개"]').value = data.data.introduce;
+            //document.querySelector('.receipe-detail-img').src = data.data.receipeImgUrl;
+            let receipeImg = document.getElementById('receipe-img');
+            console.log(data.data.receipeImgUrl);
+            let imgUrl = data.data.receipeImgUrl;
+            receipeImg.src = imgUrl;
 
-    // 재료 정보 표시
-    let ingredientContainer = document.querySelector('.form-container-ingredientitems');
-    console.log(data.data.ingredients);
+            // 재료 정보 표시
+            let ingredientContainer = document.querySelector('.form-container-ingredientitems');
+            console.log(data.data.ingredients);
 
-    data.data.ingredients.forEach((ingredient, index) => {
+            data.data.ingredients.forEach((ingredient, index) => {
 
-        let ingredientDiv = document.createElement("div");
-        ingredientDiv.className = "ingredient-div";
+                let ingredientDiv = document.createElement("div");
+                ingredientDiv.className = "ingredient-div";
 
-        let labelIng = document.createElement("label");
-        labelIng.setAttribute("for", "");
-        labelIng.id = "ingredient" + (index + 1);
-        labelIng.textContent = "재료 " + (index + 1);
+                let labelIng = document.createElement("label");
+                labelIng.setAttribute("for", "");
+                labelIng.id = "ingredient" + (index + 1);
+                labelIng.textContent = "재료 " + (index + 1);
 
-        let inputIng = document.createElement("input");
-        inputIng.type = "text";
-        inputIng.className = "receipe-detail-material";
-        inputIng.placeholder = "재료명";
-        inputIng.value = ingredient.ingredientName;
-        inputIng.disabled = true;
+                let inputIng = document.createElement("input");
+                inputIng.type = "text";
+                inputIng.className = "receipe-detail-material";
+                inputIng.placeholder = "재료명";
+                inputIng.value = ingredient.ingredientName;
+                inputIng.disabled = true;
 
-        let inputUnit = document.createElement("input");
-        inputUnit.type = "text";
-        inputUnit.className = "receipe-detail-material-unit";
-        inputUnit.placeholder = "단위";
-        inputUnit.value = ingredient.weight;
-        inputUnit.disabled = true;
+                let inputUnit = document.createElement("input");
+                inputUnit.type = "text";
+                inputUnit.className = "receipe-detail-material-unit";
+                inputUnit.placeholder = "단위";
+                inputUnit.value = ingredient.weight;
+                inputUnit.disabled = true;
 
-        ingredientDiv.appendChild(labelIng);
-        ingredientDiv.appendChild(inputIng);
-        ingredientDiv.appendChild(inputUnit);
+                ingredientDiv.appendChild(labelIng);
+                ingredientDiv.appendChild(inputIng);
+                ingredientDiv.appendChild(inputUnit);
 
-        ingredientContainer.appendChild(ingredientDiv);
+                ingredientContainer.appendChild(ingredientDiv);
 
-    });
+            });
 
-    // 순서 정보 표시
-    let orderContainer = document.querySelector('.form-container-orderitems');
-    console.log(data.data.orders);
+            // 순서 정보 표시
+            let orderContainer = document.querySelector('.form-container-orderitems');
+            console.log(data.data.orders);
 
-    data.data.orders.forEach((order, index) => {
-        let sequenceDiv = document.createElement("div");
-        sequenceDiv.className = "form-container-items"; // 클래스 수정
+            data.data.orders.forEach((order, index) => {
+                let sequenceDiv = document.createElement("div");
+                sequenceDiv.className = "form-container-items"; // 클래스 수정
 
-        let orderDiv = document.createElement("div");
-        orderDiv.className = "order-div";
+                let orderDiv = document.createElement("div");
+                orderDiv.className = "order-div";
 
-        let label = document.createElement("label");
-        label.textContent = "순서 " + (index + 1);
+                let label = document.createElement("label");
+                label.textContent = "순서 " + (index + 1);
 
-        let img = document.createElement("img");
-        img.className = "receipe-detail-sequence-img";
-        console.log(order.contentUrl);
-        img.src = order.contentUrl;
+                let img = document.createElement("img");
+                img.className = "receipe-detail-sequence-img";
+                console.log(order.contentUrl);
+                img.src = order.contentUrl;
 
-        let textarea = document.createElement("textarea");
-        textarea.className = "receipe-detail-sequence";
-        textarea.rows = "10";
-        textarea.cols = "10";
-        textarea.disabled = true;
-        textarea.value = order.content;
+                let textarea = document.createElement("textarea");
+                textarea.className = "receipe-detail-sequence";
+                textarea.rows = "10";
+                textarea.cols = "10";
+                textarea.disabled = true;
+                textarea.value = order.content;
 
-        orderDiv.appendChild(label);
-        sequenceDiv.appendChild(orderDiv); // orderDiv를 sequenceDiv의 자식으로 추가
-        sequenceDiv.appendChild(img);
-        sequenceDiv.appendChild(textarea);
+                orderDiv.appendChild(label);
+                sequenceDiv.appendChild(orderDiv); // orderDiv를 sequenceDiv의 자식으로 추가
+                sequenceDiv.appendChild(img);
+                sequenceDiv.appendChild(textarea);
 
-        orderContainer.appendChild(sequenceDiv);
-    });
-    let receipeidCheck;
-    return receipeidCheck = data.data.chefName;
+                orderContainer.appendChild(sequenceDiv);
+            });
+
+            // 레시피 작성자 체크
+            let chefName = data.data.chefName;
+            await receipeCheck(chefName);
+        }
+    } catch (error) {
+        console.error("에러 발생: " + error.message);
+    }
 }
 
-async function receipeCheck(chefName){
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + token);
+// 레시피 작성자 체크
+async function receipeCheck(chefName) {
+    try {
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + token);
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
 
-    let response = await fetch("http://localhost:8080/api/user/get/isMyReceipe/" + chefName, requestOptions);
+        let response = await fetch("http://localhost:8080/api/user/get/isMyReceipe/" + chefName, requestOptions);
 
-    if (!response.ok) {
-        const errorData = await response.json();
-        alert("데이터 조회 실패: " + JSON.stringify(errorData));
+        if (!response.ok) {
+            const errorData = await response.json();
+            alert("데이터 조회 실패: " + JSON.stringify(errorData));
+        }
+
+        let data = await response.json();
+        let check = data.data
+
+        if (check === true) {
+            deleteButton.classList.remove('hidden');
+        } else {
+            deleteButton.classList.add('hidden');
+        }
+
+        console.log(data.data)
+    } catch (error) {
+        console.error("에러 발생: " + error.message);
     }
-
-    let data = await response.json();
-    let check = data.data
-
-    if (check === true){
-        deleteButton.classList.remove('hidden');
-    }else {
-        deleteButton.classList.add('hidden');
-    }
-
-    console.log(data.data)
 }
-
 
 // 클릭시 하트상태 변경만 가능. 데이터 연결 후에 하트 상태 저장하기
 const likeButtons = document.querySelectorAll('.like');
@@ -220,70 +186,35 @@ function back() {
     console.log(referrer);
 }
 
-async function heartStatus() {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + token);
+// 삭제 버튼 클릭 이벤트 핸들러
+deleteButton.addEventListener('click', async function(event) {
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", "Bearer " + token);
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+        var requestOptions = {
+            method: 'DELETE',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
 
-    let response = await fetch("http://localhost:8080/api/user/love/check/status/" + receipeId, requestOptions);
+        let responses = await fetch("http://localhost:8080/api/user/receipe/delete/" + receipeId, requestOptions);
 
-    if (!response.ok) {
-        const errorData = await response.json();
-        alert("데이터 조회 실패: " + JSON.stringify(errorData));
+        if (!responses.ok) {
+            const errorData = await responses.json();
+            alert("데이터 조회 실패: " + JSON.stringify(errorData));
+        } else {
+            window.location.href = '/main';
+            alert("삭제 완료!");
+        }
+    } catch (error) {
+        console.error("에러 발생: " + error.message);
     }
+});
 
-    let data = await response.json();
 
-    let status = data.data.status;
-
-    return status;
-}
-
-async function love() {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer " + token);
-
-    const raw = JSON.stringify({
-        "receipeId": receipeId
-    });
-
-    const requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-
-    let response = await fetch("http://localhost:8080/api/user/love", requestOptions);
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        alert("데이터 조회 실패: " + JSON.stringify(errorData));
-    }
-}
-
-async function unlove() {
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer " + token);
-
-    var requestOptions = {
-        method: 'DELETE',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    let response = await fetch("http://localhost:8080/api/user/unlove/" + receipeId, requestOptions)
-
-    if (!response.ok) {
-        const errorData = await response.json();
-        alert("데이터 조회 실패: " + JSON.stringify(errorData));
-    }
-}
-}
+// 페이지 로드 시 상세 정보 불러오기
+window.addEventListener("DOMContentLoaded", async function () {
+    await fetchReceipeDetails();
+});
